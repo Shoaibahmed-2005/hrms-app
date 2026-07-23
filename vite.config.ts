cfba30642658cb77ea15fa18d3d04fc2558f562b
cybermajeed <abdul.zxmajeed@gmail.com>
2026-07-23 21:55:20 +0530

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     react(),
     tailwindcss(),
   ],
